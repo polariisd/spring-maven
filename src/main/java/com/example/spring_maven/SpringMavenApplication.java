3660ce2321e60d1c7d39b316406e5f878fd20a56
polariisd <152268714+polariisd@users.noreply.github.com>
2026-01-15 11:11:19 +0100

    Update SpringMavenApplication.java

--- src/main/java/com/example/spring_maven/SpringMavenApplication.java
+++ src/main/java/com/example/spring_maven/SpringMavenApplication.java
@@ -2,6 +2,8 @@ package com.example.spring_maven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 public class SpringMavenApplication {
@@ -10,4 +12,12 @@ public class SpringMavenApplication {
 		SpringApplication.run(SpringMavenApplication.class, args);
 	}
 
+}
+
+@RestController
+class HelloWorldController {
+	@GetMapping("/")
+	public String sayHello () {
+		return "Hello, World! ";
+	}
 }
