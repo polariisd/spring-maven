ebec88d6811d5f770829e8710c10ea4b281928e4
polariisd <eliot.brochard@efrei.net>
2026-01-15 10:56:56 +0100

    Initial commit - Spring Maven project

---
+++ src/test/java/com/example/spring_maven/SpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
